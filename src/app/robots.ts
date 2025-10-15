aa9e2eb34ae144c34a649beea35a3235ec8a1540
Truyện Chữ Hay <truyenchuhay.vn@gmail.com>
2025-10-15 07:38:53 +0700

    up

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from 'next'
 import { headers } from 'next/headers'
 
+export const runtime = 'edge';
+
 export default async function robots(): Promise<MetadataRoute.Robots> {
     const headersList = await headers()
     const host = await headersList.get("host")?.replace(":3000", "") || ""
